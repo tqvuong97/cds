5c67460261c425e35aaf492cd4514dd05ab0fd55
tqvuong97 <quocvuong.tqv97@gmail.com>
2019-03-20 15:26:56 +0700

    login via username and can send email reset pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  VALID_EMAIL_REGEX = /\w+@(larion|bestarion|atalink)\.com/
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "Invalid Email"}
+  # VALID_EMAIL_REGEX = /\w+@(larion|bestarion|atalink)\.com/
+  # validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "Invalid Email"}
+  devise :database_authenticatable, :authentication_keys => [:username]
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'homepage/index'
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', passwords: "users/passwords" }
   root  "homepage#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
