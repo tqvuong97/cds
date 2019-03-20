7b8c9ddcb1ba6ba2c4c77f87000e8a92f012298d
tqvuong97 <quocvuong.tqv97@gmail.com>
2019-03-20 09:40:26 +0700

    login success and change login via username instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  VALID_EMAIL_REGEX = /\w+@(larion|bestarion|atalink)\.com/
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "Invalid Email"}
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port     => 587,
+      :domain    => 'domain.com',
+      :user_name   => 'quocvuong.tqv97@gmail.com',
+      :password    => '098711344233',
+      :authentication  => :plain,
+      :enable_starttls_auto => true
+  }
+
 end
