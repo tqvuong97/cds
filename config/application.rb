b0057cea96b68bd31693a285c3a7f8b90b034d19
hoangmy121097 <hoangmy121097@gmail.com>
2019-03-21 10:46:56 +0700

    update UI login, fg pass, new pass + link font

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,13 @@ module Cds
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
+
